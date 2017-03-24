8fcba7048c99c82fd5ae17d0b5619b973a3b2385
정빈 학원 <kjb7907@naver.com>
2017-03-24 10:39:05 +0900

    스크롤감지 네비바로 변경

--- game/js_game/backObject.js
+++ game/js_game/backObject.js
@@ -1,14 +1,18 @@
  var backMotion = function(){
     var backMotionFrame;
     var objectCnt=0;
+    var backWidth=($("#testTarget").width()/500);
 
     //전진
     $('#run').mousedown(function(){
         console.log();
         backMotionFrame=setInterval(function(){
-            $( "#testTarget" ).css({ "left": "-=15px" });
-            $("#title").css({ "left": "-=15px" });
-            objectCnt++;
+            console.log(objectCnt); 
+            if(objectCnt<408){
+                $( "#testTarget" ).css({ "left":'-='+backWidth+'px' });
+                $("#title").css({ "left":'-='+backWidth+'px' });
+                objectCnt++;
+            }          
         },33);
     //
     }).mouseup(function(){
@@ -22,8 +26,8 @@
         }     
         backMotionFrame=setInterval(function(){
             if(objectCnt>0){
-                $( "#testTarget" ).css({ "left": "+=15px" });
-                $( "#title" ).css({ "left": "+=15px" });
+                $( "#testTarget" ).css({ "left":'+='+backWidth+'px' });
+                $( "#title" ).css({ "left":'+='+backWidth+'px' });
                 objectCnt--;            
             }                 
         },33);                          
