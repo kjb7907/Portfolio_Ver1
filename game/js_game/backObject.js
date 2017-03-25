3c7da6929417bdf5420b0ad9f6a9c6d38f8cbfd5
정빈 집 <kjb7907@naver.com>
2017-03-26 05:15:55 +0900

    이미지 사이즈 조정 및 게임 퍼포먼스 조정

--- game/js_game/backObject.js
+++ game/js_game/backObject.js
@@ -1,20 +1,20 @@
  var backMotion = function(){
     var backMotionFrame;
     var objectCnt=0;
-    var backWidth=($("#testTarget").width()/500.0);
+    var backWidth=($("#testTarget").width()/800.0);
     console.log($("#testTarget").width()+' backwidth:'+backWidth);
 
     //전진
     $('#run').mousedown(function(){
         console.log();
         backMotionFrame=setInterval(function(){
-            console.log(objectCnt); 
-            if(objectCnt<408){
+            //console.log(objectCnt); 
+            if(objectCnt<640){
                 $( "#testTarget" ).css({ "left":'-='+backWidth+'px' });
                 $("#title").css({ "left":'-='+backWidth+'px' });
                 objectCnt++;
             }          
-        },33);
+        },20);
     //
     }).mouseup(function(){
         clearInterval(backMotionFrame);
@@ -31,7 +31,7 @@
                 $( "#title" ).css({ "left":'+='+backWidth+'px' });
                 objectCnt--;            
             }                 
-        },33);                          
+        },20);                          
     })
     //
     .mouseup(function(){
